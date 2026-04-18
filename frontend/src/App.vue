5097cad6a284bdf2c0c1137785824a160f979bbf
Grazie <1472858143@qq.com>
2026-04-18 14:15:47 +0800

    feat: 清理前端示例页面

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
+<template>
+  <div> 
+    Inventory Frontend
+  </div>
+</template>
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <HelloWorld />
-</template>
+<style>
+</style>
